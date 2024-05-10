6f5bab206a60df5779be7b29568d7642c7271cb9
Eldar Bukeyev <bukeevel@gmail.com>
2024-05-10 13:08:26 +0300

    Added header and Error response

--- main.go
+++ main.go
@@ -13,9 +13,7 @@ func main() {
 		w.Write([]byte("Форма показа заметки"))
 	})
 
-	mux.HandleFunc("/snippet/create", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Форма создания заметки"))
-	})
+	mux.HandleFunc("/snippet/create", createSnippet)
 
 	log.Println("Запус веб-сервера на http://127.0.0.1:4000")
 	err := http.ListenAndServe(":4000", mux)
@@ -28,4 +26,15 @@ func home(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	w.Write([]byte("Привет из SnippetBox"))
+}
+
+func createSnippet(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method not allowed.", 405)
+		return
+	}
+
+	w.Write([]byte("Форма создания заметки"))
 }
