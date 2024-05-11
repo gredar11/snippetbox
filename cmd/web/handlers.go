a4e176a1f529b849e96e6a7885741e9e0d923d92
Eldar Bukeyev <bukeevel@gmail.com>
2024-05-11 13:00:57 +0300

    Added stack trace to logs and helper methods

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -9,32 +9,32 @@ import (
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 	files := []string{
 		"./ui/html/home.page.tmpl",
 		"./ui/html/base.layout.tmpl",
-		"./ui/html/footer.partial1.tmpl",
+		"./ui/html/footer.partial.tmpl",
 	}
 
 	ts, err := template.ParseFiles(files...)
 
 	if err != nil {
-		app.handleError(w, err)
+		app.serverError(w, err)
 		return
 	}
 
 	err = ts.Execute(w, nil)
 	if err != nil {
-		app.handleError(w, err)
+		app.serverError(w, err)
 	}
 }
 
 func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
-		http.NotFound(w, r)
+		app.notFound(w)
 		return
 	}
 
@@ -44,7 +44,7 @@ func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
 func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
-		http.Error(w, "Метод не дозволен", 405)
+		app.clientError(w, http.StatusMethodNotAllowed)
 		return
 	}
 
